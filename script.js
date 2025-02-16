49941a6d02d417037f2bd4d6db6718fb33429ae4
Suv22 <sakshivajre94@gmail.com>
2025-02-16 22:03:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function openBottomSheet(sheetNumber) {
+    // document.getElementById("bottomSheet").style.bottom = "0";
+    document.getElementById("sheetNumber" + sheetNumber).style.bottom = "0"; 
+    // document.getElementById("sheetNumber" + sheetNumber).classList.add("active");
+}
+
+function closeBottomSheet(sheetNumber) {
+    document.getElementById("sheetNumber" + sheetNumber).style.bottom = "-100%";
+    // document.getElementById("sheet" + sheetNumber).classList.remove("active");
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("main-img");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
