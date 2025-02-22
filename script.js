9047a2ec901edc2384dbf28995586017adda0732
Suv22 <sakshivajre94@gmail.com>
2025-02-22 15:19:40 +0530

    padding changes

--- script.js
+++ script.js
@@ -11,17 +11,37 @@
 
 
 
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("item3");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   slides[slideIndex-1].style.display = "block";  
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
 let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("main-img");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  let slides = document.getElementsByClassName("item3");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.visibility = "hidden";
+    slides[i].style.opacity = "0";
+    slides[i].style.position = "absolute"; /* Prevents shifting */
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
+  if (slideIndex > slides.length) { slideIndex = 1; }  
+
+  slides[slideIndex - 1].style.visibility = "visible";
+  slides[slideIndex - 1].style.opacity = "1";
+  slides[slideIndex - 1].style.position = "relative"; 
+
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
